b820a22fdb3019e56c1d98291ade8469521d2441
Sanskriti029 <sanskritikhandelwal029@gmail.com>
2026-07-07 13:08:30 +0530

    comit pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,17 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// // import tailwindcss from '@tailwindcss/vite'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), ],
+// })
+// tailwindcss()
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), ],
+  plugins: [react()],
+  base: '/Lubricant-website/', // Replace with your GitHub repository name
 })
-// tailwindcss()
